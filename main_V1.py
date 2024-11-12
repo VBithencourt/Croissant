71a1a24cf950e319cdd1b786601885540e8d621c
João Victor <joaov04.2024@gmail.com>
2024-11-12 16:37:31 -0300

    Tela Tutorial_V1

--- main_V1.py
+++ main_V1.py
@@ -1,6 +1,7 @@
 import pygame
 from mapa_F1 import mapa_1
 from tela_inicial import mostrar_tela_inicial
+from tela_tutorial import mostrar_tutorial
 
 pygame.init()
 
@@ -16,29 +17,35 @@ altura_tile = 20
 
 mostrar_tela_inicial(janela)
 
+tutorial_ativo = False
 jogo = True
+
 
 while jogo:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jogo = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_t:  # Verifica se a tecla 'T' foi pressionada
+                tutorial_ativo = not tutorial_ativo  # Alterna entre ativar e desativar o tutorial
+
 
     janela.fill((255, 255, 255))
 
-# Desenha o mapa 1
-    for linha in range(len(mapa_1)):
-        for coluna in range(len(mapa_1[linha])):
-            x = coluna * largura_tile
-            y = linha * altura_tile
-            if mapa_1[linha][coluna] == 1:
-                janela.blit(con, (x, y))
-            elif mapa_1[linha][coluna] == 0:
-                janela.blit(P_lisa, (x, y))
-            elif mapa_1[linha][coluna] == 2:
-                janela.blit(grama, (x, y))
-
-
-    
+    if tutorial_ativo:
+        mostrar_tutorial(janela)
+    else:
+        for linha in range(len(mapa_1)):
+            for coluna in range(len(mapa_1[linha])):
+                x = coluna * largura_tile
+                y = linha * altura_tile
+                if mapa_1[linha][coluna] == 1:
+                    janela.blit(con, (x, y))
+                elif mapa_1[linha][coluna] == 0:
+                    janela.blit(P_lisa, (x, y))
+                elif mapa_1[linha][coluna] == 2:
+                    janela.blit(grama, (x, y))
+                    
     pygame.display.update()
 
 pygame.quit()
