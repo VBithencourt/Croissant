ccb8a0981d94a23bb4ef27c71ae2b788a4eeebc0
VBithencourt <viniciussb3@al.insper.edu.br>
2024-11-18 18:58:21 -0300

    mapa2

--- main_V2.py
+++ main_V2.py
@@ -1,11 +1,31 @@
+
 import pygame
-from mapa_F1 import *
-from tela_inicial import mostrar_tela_inicial
-from tela_tutorial import mostrar_tutorial
-#from musica import *
-from player import *
+from mapa_F1 import mapa_3
+from tela_inicial import * 
+from tela_tutorial import *
 from player2 import *
 from tela_final_ import *
+
+con = pygame.image.load('Coisinhas/pedragrama.jpg').convert()
+Va1 = pygame.image.load('Coisinhas/cid.jpg').convert()
+grama = pygame.image.load('Coisinhas/grama.png').convert()
+P_lisa = pygame.image.load('Coisinhas/Fundoazul.jpg').convert()
+P1 = pygame.image.load('Coisinhas/p1.png').convert()
+P2 = pygame.image.load('Coisinhas/p2.png').convert()
+P3 = pygame.image.load('Coisinhas/p5.png').convert()
+P4 = pygame.image.load('Coisinhas/p4.png').convert()
+
+pygame.mixer.music.load('musica/soundtrack.mpeg')
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+efeito_sonoro = pygame.mixer.Sound('musica/botão.mpeg')
+efeito_sonoro.set_volume(0.4) 
+def tocar_efeito_sonoro():
+    efeito_sonoro.play()
+    pygame.time.set_timer(pygame.USEREVENT, 1500)
+
+
 
 mostrar_tela_inicial(janela)
 
@@ -16,21 +36,18 @@ game_over = False
 tela_final = False
 
 mortes_player = 0
-mortes_player2 = 0
+
 
 player_chegou_no_4 = False
-player2_chegou_no_4 = False
 
 
 font_score = pygame.font.Font(None, 36)
-x_inicial = 1300 
-y_inicial =700
 
-# player = Player(x_inicial, y_inicial, 20, 20)  # Use as variáveis x_inicial e y_inicial
-# player2 = Player2(x_inicial + 20, y_inicial, 20, 20)  # Para player2, use o x_inicial + 20
-
+font_score = pygame.font.Font(None, 36)
+x_inicial = 1300
+y_inicial = 700
 player = Player(x_inicial, y_inicial, 20, 20)
-player2 = Player2(x_inicial, y_inicial, 20, 20)
+
 while jogo:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,44 +55,44 @@ while jogo:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_t:  # Ativa/Desativa o tutorial
                 tutorial_ativo = not tutorial_ativo
-                #tocar_efeito_sonoro()
+                tocar_efeito_sonoro()
             if event.key == pygame.K_r and game_over:  # Reinicia o jogo
                 # Restaura o mapa e a posição dos jogadores
-                mapa_2 = [linha[:] for linha in mapa_2]  # Restaura o mapa
-                player = Player(800, 30, 20, 20)  # Cria o player na posição inicial
-                player2 = Player2(820, 30, 20, 20)  # Cria o player2 na posição inicial
+                mapa_3 = [linha[:] for linha in mapa_3]  # Restaura o mapa
+                player = Player(1300, 700, 20, 20)  # Cria o player na posição inicial
+                
                 game_over = False
                 som_tocado = False  # Reinicia o som
                 player_chegou_no_4 = False  # Reinicia o status de chegada
-                player2_chegou_no_4 = False  # Reinicia o status de chegada
-                #tocar_efeito_sonoro()  # Toca o som novamente
+                
+                tocar_efeito_sonoro()  # Toca o som novamente
 
-        #if event.type == pygame.USEREVENT:
-            #efeito_sonoro.stop()
+        if event.type == pygame.USEREVENT:
+            efeito_sonoro.stop()
 
-        if game_over:
-            # Se o jogo acabou, mostra a tela de Game Over
-            font = pygame.font.Font(None, 74)
-            texto = font.render("GAME OVER", True, (255, 0, 0))
-            janela.blit(texto, (500, 300))
-
-            texto_mortes_player = font_score.render(f'Mortes Player 1: {mortes_player}', True, (0, 0, 0))
-            texto_mortes_player2 = font_score.render(f'Mortes Player 2: {mortes_player2}', True, (0, 0, 0))
-            janela.blit(texto_mortes_player, (500, 480))
-            janela.blit(texto_mortes_player2, (500, 500))
-
-            font_button = pygame.font.Font(None, 36)
-            button = font_button.render("Pressione R para Reiniciar", True, (0, 0, 0))
-            janela.blit(button, (500, 400))
-        elif tela_final:
-        # Mostra a tela final e espera o jogador pressionar "Esc" para sair
-            if mostrar_final(janela):
-                jogo = False  # Encerra o jogo se "Esc" for pressionado
+    if game_over:
+        # Se o jogo acabou, mostra a tela de Game Over
+        font = pygame.font.Font(None, 74)
+        texto = font.render("GAME OVER", True, (255, 0, 0))
+        janela.blit(texto, (500, 300))
 
         texto_mortes_player = font_score.render(f'Mortes Player 1: {mortes_player}', True, (0, 0, 0))
-        texto_mortes_player2 = font_score.render(f'Mortes Player 2: {mortes_player2}', True, (0, 0, 0))
+        
+        janela.blit(texto_mortes_player, (500, 480))
+        
+
+        font_button = pygame.font.Font(None, 36)
+        button = font_button.render("Pressione R para Reiniciar", True, (0, 0, 0))
+        janela.blit(button, (500, 400))
+    elif tela_final:
+        # Mostra a tela final e espera o jogador pressionar "Esc" para sair
+        if mostrar_final(janela):
+            jogo = False  # Encerra o jogo se "Esc" for pressionado
+
+        texto_mortes_player = font_score.render(f'Mortes Player 1: {mortes_player}', True, (0, 0, 0))
+       
         janela.blit(texto_mortes_player, (500, 400))
-        janela.blit(texto_mortes_player2, (500, 440))
+        
 
 
         # Botão para reiniciar
@@ -85,9 +102,8 @@ while jogo:
     else:
         # Jogo em andamento
         player.handle_input()
-        player.fisica()
-        player2.handle_input()
-        player2.fisica()
+        player.fisica3()
+
 
         janela.fill((255, 255, 255))
 
@@ -95,40 +111,56 @@ while jogo:
             mostrar_tutorial(janela)
         else:
             if not som_tocado:
-                #tocar_efeito_sonoro()
+                tocar_efeito_sonoro()
                 som_tocado = True
-            for linha in range(len(mapa_2)):
-                for coluna in range(len(mapa_2[linha])):
+            for linha in range(len(mapa_3)):
+                for coluna in range(len(mapa_3[linha])):
                     x = coluna * largura_tile
                     y = linha * altura_tile
-                    if mapa_2[linha][coluna] == 1:
+                    if mapa_3[linha][coluna] == 1:
                         janela.blit(con, (x, y))
-                    elif mapa_2[linha][coluna] == 0:
+                    elif mapa_3[linha][coluna] == 0:
                         janela.blit(P_lisa, (x, y))
-                    elif mapa_2[linha][coluna] == 2:
-                        janela.blit(grama, (x, y))
+                    elif mapa_3[linha][coluna] == 3:
+                        janela.blit(Va1, (x, y))
+                    elif mapa_3[linha][coluna] == 'p1':
+                        janela.blit(P1, (x, y))
+                    elif mapa_3[linha][coluna] == 'p2':
+                        janela.blit(P2, (x, y))
+                    elif mapa_3[linha][coluna] == 'p3':
+                        janela.blit(P3, (x, y))
+                    elif mapa_3[linha][coluna] == 'p4':
+                        janela.blit(P4, (x, y))
 
             player.draw(janela)
-            player2.draw(janela)
+      
 
             # Verifica se algum jogador pisou no tile "3"
             player_pos = (player.rect.x // largura_tile, player.rect.y // altura_tile)
-            player2_pos = (player2.rect.x // largura_tile, player2.rect.y // altura_tile)
 
-            if mapa_2[player_pos[1]][player_pos[0]] == 3:
-                game_over = True  # Acabou o jogo, o player pisou no tile 3
-                mortes_player += 1  # Incrementa a morte do player
 
-            if mapa_2[player2_pos[1]][player2_pos[0]] == 3:
-                game_over = True  # Acabou o jogo, o player2 pisou no tile 3
-                mortes_player2 += 1  # Incrementa a morte do player2
+            # if mapa_1[player_pos[1]][player_pos[0]] == 3:
+            #     game_over = True  # Acabou o jogo, o player pisou no tile 3
+            #     mortes_player += 1  # Incrementa a morte do player
 
-            if mapa_2[player_pos[1]][player_pos[0]] == 3 or mapa_2[player2_pos[1]][player2_pos[0]] == 3:
-                game_over = True  # Acabou o jogo, um dos jogadores pisou no tile 3
+
+            # if mapa_1[player_pos[1]][player_pos[0]] == 3: #or mapa_1[player2_pos[1]][player2_pos[0]] == 3:
+            #     game_over = True  # Acabou o jogo, um dos jogadores pisou no tile 3
+
+            if mapa_3[player_pos[1]][player_pos[0]] == 3:
+                    game_over = True  # Acabou o jogo, o player pisou no tile 3
+                    mortes_player += 1  # Incrementa a morte do player
+
+            if mapa_3[player_pos[1]][player_pos[0]] == 3: #mapa_3[player2_pos[1]][player2_pos[0]] == 3:
+                    game_over = True  # Acabou o jogo, um dos jogadores pisou no tile 3
             
-            if mapa_2[player_pos[1]][player_pos[0]] == 4 or mapa_2[player2_pos[1]][player2_pos[0]] == 4:
-                tela_final = True  # Vai para a tela final do jogo
             
+            if mapa_3[player_pos[1]][player_pos[0]] == 'p1' :# and mapa_3[player2_pos[1]][player2_pos[0]] == 'p1':
+                    tela_final = True  # Vai para a tela final do jogo
+            
+
+            
+
              
 
     pygame.display.update()
--- player2.py
+++ player2.py
@@ -1,9 +1,6 @@
 import pygame
 from mapa_F1 import *
 
-largura_tile = 20
-altura_tile = 20
-
 pygame.init()
 
 janela = pygame.display.set_mode((1400, 800))
@@ -15,24 +12,30 @@ Va1 = pygame.image.load('Coisinhas/Vines-acid.png').convert()
 grama = pygame.image.load('Coisinhas\grama.png').convert()
 P_lisa = pygame.image.load('Coisinhas\ParedeLisa.png').convert()
 
-class Player2:
-    def __init__(self, x, y, width, height):
 
+largura_tile = 20
+altura_tile = 20
+
+class Player:
+    def __init__(self, x, y, width, height):
         self.frames = [
-            pygame.image.load('Coisinhas/spprite1.png').convert_alpha(),
-            pygame.image.load('Coisinhas/spprite2.png').convert_alpha(),
-            pygame.image.load('Coisinhas/spprite3.png').convert_alpha(),
-            pygame.image.load('Coisinhas/spprite4.png').convert_alpha(),
-            pygame.image.load('Coisinhas/spprite5.png').convert_alpha(),
-            pygame.image.load('Coisinhas/spprite6.png').convert_alpha()
+            pygame.image.load('Coisinhas/sprite1.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite2.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite3.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite4.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite5.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite6.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite7.png').convert_alpha(),
+            pygame.image.load('Coisinhas/sprite8.png').convert_alpha()
         ]
 
         self.image = self.frames[0]
         self.rect = pygame.Rect(x, y, width, height)
+
         self.speed = 0.8
         self.velocity_y = 0
         self.gravity = 0.2
-        self.jump_strength = -7
+        self.jump_strength = -6.75
         self.on_ground = False  # Inicialmente, o personagem não está no chão
 
         
@@ -42,42 +45,54 @@ class Player2:
         self.image = self.frames[self.frame_index]  # Começa com o primeiro quadro
         self.facing_right = True  # O personagem começa olhando para a direita
 
+
+
     def handle_input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.movimentacao(-self.speed, 0)  # Movimenta para a esquerda
             self.facing_right = False  # O personagem está indo para a esquerda
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.movimentacao(self.speed, 0)   # Movimenta para a direita
             self.facing_right = True  # O personagem está indo para a direita
-        if keys[pygame.K_w] and self.on_ground: # Pulo só se estiver no chão
+        if keys[pygame.K_UP] and self.on_ground: # Pulo só se estiver no chão
             self.velocity_y = self.jump_strength
             self.on_ground = False
 
     def movimentacao(self, dx, dy):
         # Movimentação horizontal
         self.rect.x += dx
-        if self.checa_colisao2():
+        if self.checa_colisao3():
             self.rect.x -= dx  # Reverte o movimento se houver colisão
 
         # Movimentação vertical
         self.rect.y += dy
-        if self.checa_colisao2():
+        if self.checa_colisao3():
             self.rect.y -= dy  # Reverte o movimento se houver colisão
             if dy > 0:  # Se o personagem estava caindo
                 self.on_ground = True
                 self.velocity_y = 0
             elif dy < 0:  # Se o personagem estava subindo
                 self.velocity_y = 0
+    def checa_colisao3(self):
+        # Verificando colisões com os blocos sólidos no mapa
+        for linha in range(len(mapa_3)):
+            for coluna in range(len(mapa_3[linha])):
+                if mapa_3[linha][coluna] == 1:  # Blocos sólidos (terreno)
+                    bloco = pygame.Rect(coluna * largura_tile, linha * altura_tile, largura_tile, altura_tile)
+                    if self.rect.colliderect(bloco):
+                        return True
+        return False
+    
 
-    def fisica(self):
+    def fisica3(self):
         if not self.on_ground:  # Aplica gravidade somente se não estiver no chão
             self.velocity_y += self.gravity
         self.rect.y += self.velocity_y
 
         # Verificando colisão com o chão
-        if self.checa_colisao2():
+        if self.checa_colisao3():
             self.rect.y -= self.velocity_y  # Reverte o movimento vertical
             self.velocity_y = 0
             self.on_ground = True  # O personagem está tocando o chão
@@ -85,6 +100,7 @@ class Player2:
             self.on_ground = False  # O personagem não está tocando o chão
 
         self.update()
+        # Verificando colisão com o chão
 
     def update(self):
         now = pygame.time.get_ticks()
@@ -102,13 +118,6 @@ class Player2:
         janela.blit(self.image, (self.rect.x, self.rect.y - self.image.get_height() + self.rect.height))
 
 
-    def checa_colisao2(self):
-        # Verificando colisões com os blocos sólidos no mapa
-        for linha in range(len(mapa_2)):
-            for coluna in range(len(mapa_2[linha])):
-                if mapa_2[linha][coluna] == 1:  # Blocos sólidos (terreno)
-                    bloco = pygame.Rect(coluna * largura_tile, linha * altura_tile, largura_tile, altura_tile)
-                    if self.rect.colliderect(bloco):
-                        return True
-        return False
+
+
     
