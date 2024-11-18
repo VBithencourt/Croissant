8d9d861c6c641dab1e665f85594572fbbf188aad
VBithencourt <viniciussb3@al.insper.edu.br>
2024-11-18 01:54:21 -0300

    Add files via upload

---
+++ Croissant-main/main_V1.py
@@ -0,0 +1,66 @@
+import pygame
+from mapa_F1 import mapa_1
+from tela_inicial import mostrar_tela_inicial
+from tela_tutorial import mostrar_tutorial
+from musica import *
+from player import *
+from player2 import *
+
+pygame.init()
+
+janela = pygame.display.set_mode((1400,800))
+pygame.display.set_caption('Insper challengers: Em busca do croissant de chocolate.')
+
+# Carregar imagens
+con = pygame.image.load('Coisinhas/concreto.png').convert()
+Va1 = pygame.image.load('Coisinhas/Vines-acid.png').convert()
+grama = pygame.image.load('Coisinhas\grama.png').convert()
+P_lisa = pygame.image.load('Coisinhas\ParedeLisa.png').convert()
+
+mostrar_tela_inicial(janela)
+
+tutorial_ativo = False
+jogo = True
+som_tocado = False
+
+while jogo:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogo = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_t:  # Verifica se a tecla 'T' foi pressionada
+                tutorial_ativo = not tutorial_ativo  # Alterna entre ativar e desativar o tutorial
+                tocar_efeito_sonoro()
+        if event.type == pygame.USEREVENT:
+            efeito_sonoro.stop()
+
+    player.handle_input()
+    player.fisica()
+    player2.handle_input()
+    player2.fisica()
+
+    janela.fill((255, 255, 255))
+
+    if tutorial_ativo:
+        mostrar_tutorial(janela)
+    else:
+        if not som_tocado: 
+            tocar_efeito_sonoro()
+            som_tocado = True
+        for linha in range(len(mapa_1)):
+            for coluna in range(len(mapa_1[linha])):
+                x = coluna * largura_tile
+                y = linha * altura_tile
+                if mapa_1[linha][coluna] == 1:
+                    janela.blit(con, (x, y))
+                elif mapa_1[linha][coluna] == 0:
+                    janela.blit(P_lisa, (x, y))
+                elif mapa_1[linha][coluna] == 2:
+                    janela.blit(grama, (x, y))
+
+        player.draw(janela)
+        player2.draw(janela)
+
+    pygame.display.update()
+
+pygame.quit()
---
+++ Croissant-main/tela_inicial.py
@@ -0,0 +1,39 @@
+import pygame
+pygame.font.init()
+fonte_personalizada = pygame.font.Font("Coisinhas/Fontejogo.ttf", 190)
+imagem_fundo = pygame.image.load("Coisinhas/Fundo2.jpeg")
+
+def mostrar_tela_inicial(janela):
+    iniciar = False
+    while not iniciar:
+        janela.blit(imagem_fundo, (0, 0))
+        texto = "Pressione qualquer tecla para iniciar"
+        texto_preto = fonte_personalizada.render(texto, True, (0, 0, 0))  # Texto preto
+        texto_branco = fonte_personalizada.render(texto, True, (255, 255, 255))  # Texto branco (borda)
+        texto_rect = texto_preto.get_rect(center=(1400 // 2, 800 // 2 + 330))
+        janela.blit(texto_branco, (texto_rect.x - 2, texto_rect.y))  # Esquerda
+        janela.blit(texto_branco, (texto_rect.x + 2, texto_rect.y))  # Direita
+        janela.blit(texto_branco, (texto_rect.x, texto_rect.y - 2))  # Cima
+        janela.blit(texto_branco, (texto_rect.x, texto_rect.y + 2))  # Baixo
+        janela.blit(texto_branco, (texto_rect.x - 2, texto_rect.y - 2))  # Canto superior esquerdo
+        janela.blit(texto_branco, (texto_rect.x + 2, texto_rect.y - 2))  # Canto superior direito
+        janela.blit(texto_branco, (texto_rect.x - 2, texto_rect.y + 2))  # Canto inferior esquerdo
+        janela.blit(texto_branco, (texto_rect.x + 2, texto_rect.y + 2))  # Canto inferior direito
+
+        janela.blit(texto_preto, texto_rect)
+
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                iniciar = True
+
+
+
+       
+
+        
